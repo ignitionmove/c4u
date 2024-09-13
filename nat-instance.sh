15277551c071084244c4c3cc570c0fb04e216d14
ignitionmove <ignitionmove@gmail.com>
2024-09-13 11:32:40 +0900

    Update nat-instance.sh

--- nat-instance.sh
+++ nat-instance.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 sysctl -p /etc/sysctl.conf
-yum install -y iptables-sevices
+yum install -y iptables-services
 systemctl start iptables.service
 systemctl enable iptables.service
 iptables -F
